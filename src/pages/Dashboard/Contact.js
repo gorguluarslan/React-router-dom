88d2aec8c4818c87c56d052f1d03d0d239f82669
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-12-19 19:24:24 +0300

    update: add useformik hook

--- src/pages/Dashboard/Contact.js
+++ src/pages/Dashboard/Contact.js
@@ -1,51 +1,64 @@
 import React from "react";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 
 const Contact = () => {
+  const { handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      message: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div>
       <h1>Contact</h1>
 
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {({ handleSubmit, handleChange }) => (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input
-              id="firstName"
-              name="firstName"
-              placeholder="Jane"
-              onChange={handleChange("firstName")}
-            />
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            placeholder="Jane"
+            onChange={handleChange("firstName")}
+          />
+        </div>
 
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              id="lastName"
-              name="lastName"
-              placeholder="Doe"
-              onChange={handleChange("lastName")}
-            />
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            placeholder="Doe"
+            onChange={handleChange("lastName")}
+          />
+        </div>
 
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              name="email"
-              placeholder="jane@acme.com"
-              type="email"
-              onChange={handleChange("e-mail")}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        )}
-      </Formik>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+            onChange={handleChange("e-mail")}
+          />
+        </div>
+        <div>
+          <label htmlFor="Message">Message</label>
+
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your message..."
+            onChange={handleChange("message")}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
