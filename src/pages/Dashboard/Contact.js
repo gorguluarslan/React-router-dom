9432db87cc69279e1deb22182d958f820b17ddc4
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2022-12-19 16:15:33 +0300

    update: modifying formik library to use with html tags

--- src/pages/Dashboard/Contact.js
+++ src/pages/Dashboard/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik } from "formik";
 
 const Contact = () => {
   return (
@@ -12,27 +12,39 @@ const Contact = () => {
           lastName: "",
           email: "",
         }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
+        onSubmit={(values) => {
+          console.log(values);
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              id="firstName"
+              name="firstName"
+              placeholder="Jane"
+              onChange={handleChange("firstName")}
+            />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              id="lastName"
+              name="lastName"
+              placeholder="Doe"
+              onChange={handleChange("lastName")}
+            />
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-          <button type="submit">Submit</button>
-        </Form>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+              onChange={handleChange("e-mail")}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </Formik>
     </div>
   );
